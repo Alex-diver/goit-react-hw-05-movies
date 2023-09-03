723e900c1165f07986f8877c89de15b493f2aa37
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-09-03 20:42:18 +0300

    movies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'modern-normalize';
-import './index.css';
-
 import { App } from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// basename = "/goit-react-hw-05-movies";
